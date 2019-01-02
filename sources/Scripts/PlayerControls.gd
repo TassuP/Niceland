d6cab32dab074ecfeb583938410ccfbabe52c37e
Tapio Pyrhönen <tapio.pyrhonen@gmail.com>
2019-01-02 13:06:41 +0200

    Toggle invert y by pressing 'y'

--- sources/Scripts/PlayerControls.gd
+++ sources/Scripts/PlayerControls.gd
@@ -36,9 +36,9 @@ func _physics_process(delta):
 		target_trans.translate(Vector3(1.0, 0.0, 0.0) * spd)
 		
 	if(Input.is_action_just_released("ui_scroll_up")):
-		target_trans.translate(Vector3(0.0, 10.0, 0.0) * spd)
+		target_trans.translate(Vector3(0.0, 2.0, 0.0) * spd)
 	if(Input.is_action_just_released("ui_scroll_down")):
-		target_trans.translate(Vector3(0.0, -10.0, 0.0) * spd)
+		target_trans.translate(Vector3(0.0, -2.0, 0.0) * spd)
 	
 	# Smooth apply
 	transform = transform.interpolate_with(target_trans.transform, clamp(delta * 2.0, 0.0, 1.0))
@@ -55,6 +55,11 @@ func _physics_process(delta):
 		target_trans.transform.origin += r
 
 func _input(event):
+	
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_Y:
+			invert_mouse = !invert_mouse
+	
 	# Mouselook
 	if (event is InputEventMouseMotion):
 		var mm = event.relative / 200.0
